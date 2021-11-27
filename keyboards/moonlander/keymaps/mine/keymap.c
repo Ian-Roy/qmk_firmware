6a338309a22e96f55971c9ec4ba2be9536017bb2
Your Name <you@example.com>
2021-11-27 16:42:01 -0500

    latest

--- keyboards/moonlander/keymaps/mine/keymap.c
+++ keyboards/moonlander/keymaps/mine/keymap.c
@@ -25,18 +25,65 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
+
 enum layers {
     BASE,  // default layer
     SYMB,  // symbols
     MDIA,  // media keys
     DPAD, // navigational layer 
     SCRL,
+    AWAY,
 };
 
 enum custom_keycodes {
     VRSN = ML_SAFE_RANGE,
 };
 
+
+void macroTogKey1(qk_tap_dance_state_t *state, void *user_data) {
+  keyrecord_t kr;
+  if ( state->count > 3 )
+    return;
+
+  else if (state->count == 1)
+  {
+    kr.event.pressed = false;
+    process_dynamic_macro( DYN_MACRO_PLAY1, &kr );
+  }
+  else if (state->count == 2)
+  {
+    kr.event.pressed = true;
+    process_dynamic_macro( DYN_REC_STOP, &kr );
+  }
+  else if (state->count == 3)
+  {
+    kr.event.pressed = false;
+    process_dynamic_macro( DYN_REC_START1, &kr );
+  }
+}
+
+void macroTogKey2(qk_tap_dance_state_t *state, void *user_data) {
+  keyrecord_t kr;
+
+  if ( state->count > 3 )
+    return;
+
+  else if (state->count == 1)
+  {
+    kr.event.pressed = false;
+    process_dynamic_macro( DYN_MACRO_PLAY2, &kr );
+  }
+  else if (state->count == 2)
+  {
+    kr.event.pressed = true;
+    process_dynamic_macro( DYN_REC_STOP, &kr );
+  }
+  else if (state->count == 3)
+  {
+    kr.event.pressed = false;
+    process_dynamic_macro( DYN_REC_START2, &kr );
+  }
+}
 // Tap Dance declarations
 enum {
     TD_C_X,
@@ -52,11 +99,12 @@ enum {
     TD_0_F10 ,
     TD_EQ_F11 ,
     TD_PIPE_F12 ,
+    TD_MAC2 ,
+    TD_MAC1 ,
     };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
     [TD_C_X] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_C),LCTL(KC_X)),
     [TD_1_F1] = ACTION_TAP_DANCE_DOUBLE(KC_1 ,KC_F1),
     [TD_2_F2] = ACTION_TAP_DANCE_DOUBLE(KC_2 ,KC_F2),
@@ -69,12 +117,22 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_9_F9] = ACTION_TAP_DANCE_DOUBLE(KC_9 ,KC_F9),
     [TD_0_F10] = ACTION_TAP_DANCE_DOUBLE(KC_0 ,KC_F10),
     [TD_EQ_F11] = ACTION_TAP_DANCE_DOUBLE(KC_EQL ,KC_F11),
-    [TD_PIPE_F12] = ACTION_TAP_DANCE_DOUBLE(KC_PIPE ,KC_F12)
+    [TD_PIPE_F12] = ACTION_TAP_DANCE_DOUBLE(KC_PIPE ,KC_F12),
+    [TD_MAC1] = ACTION_TAP_DANCE_FN(macroTogKey1),
+    [TD_MAC2] = ACTION_TAP_DANCE_FN(macroTogKey2),
 
 
 };
 
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPACE, KC_DELETE);
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
+};
+
 // Add tap dance item to your keymap in place of a keycode
+// TD(TD_EQ_F11), 
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -83,8 +141,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_DEL, KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,  KC_LBRC,           KC_RBRC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
 LT(DPAD,KC_BSPC), KC_A,  KC_S,    KC_D,    KC_F,    KC_G,  KC_LPRN,           KC_RPRN,  KC_H,    KC_J,    KC_K,    KC_L,    LT(SCRL, KC_SCLN), LT(DPAD, KC_QUOT),
   LCTL(KC_Y), LCTL_T(KC_Z),KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), LT(SYMB,KC_TILDE),
- LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),RGB_M_K, TG(DPAD),  LCTL(KC_Z),    TD(TD_C_X),          RGB_TOG,   RGB_TOG, DM_REC2, DM_REC1, DM_RSTP,
-                                    LSFT_T(KC_SPC),  KC_BSPC, KC_DEL,           LCTL(KC_V),     KC_ENT,      RSFT_T(KC_TAB)
+ LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),TG(AWAY), TG(DPAD),  LCTL(KC_Z),    TD(TD_C_X),          RGB_TOG,   RGB_TOG, RGB_TOG, TD(TD_MAC2), TD(TD_MAC1),
+                                    LSFT_T(KC_SPC), KC_BSPC,   KC_LALT,          LCTL(KC_V),     KC_ENT,      RSFT_T(KC_TAB)
 
     ),
     // [ORIG] = LAYOUT_moonlander(
@@ -94,6 +152,14 @@ LT(DPAD,KC_BSPC), KC_A,  KC_S,    KC_D,    KC_F,    KC_G,  KC_LPRN,           KC
     //     KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
     // LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,  LALT_T(KC_APP),    RCTL_T(KC_ESC),   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
     //                                         KC_SPC,  KC_BSPC, KC_LGUI,           KC_LALT,  KC_TAB,  KC_ENT
+    // ),
+    // [blank] = LAYOUT_moonlander(
+    //     _______, _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, _______,
+    //     _______, _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, _______,
+    //     _______, _______, _______, _______, _______, _______, _______,                              _______, _______, _______, _______, _______, _______,
+    //     _______, _______, _______, _______, _______, _______,                                       _______, _______, _______, _______, _______, _______,
+    //     _______, _______, _______, _______, _______,          _______,                     _______,          _______, _______, _______, _______, _______,
+    //                                         _______, _______, _______,                     _______, _______, _______,
     // ),
     [SYMB] = LAYOUT_moonlander(
         VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
@@ -124,10 +190,18 @@ LT(DPAD,KC_BSPC), KC_A,  KC_S,    KC_D,    KC_F,    KC_G,  KC_LPRN,           KC
         _______,_______,_______, _______, _______, _______, _______,           KC_PIPE, _______, _______, _______, _______, _______, RESET,
         DM_PLY2, _______, KC_PGUP, KC_UP, KC_PGDOWN, _______, _______,           KC_EQL ,KC_7,    KC_8,    KC_9,KC_ASTR, _______, _______,
         _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END, _______,           KC_DEL ,KC_4,    KC_5,    KC_6,KC_PLUS, _______, KC_MPLY,
-        _______, _______, _______, _______, _______, _______,                            KC_1,    KC_2,    KC_3, KC_MINS, _______, _______,
+      _______, _______,LCTL(KC_1),LCTL(KC_GRAVE),LCTL(KC_2), _______,                            KC_1,    KC_2,    KC_3, KC_MINS, _______, _______,
         _______, _______, _______, _______, _______,         _______,            KC_BSPC,         KC_DOT, KC_0, KC_MUTE, _______, TG(BASE),
                                             _______, _______, _______,           KC_COMM, KC_ENT , KC_TAB
     ),
+    [AWAY] = LAYOUT_moonlander(
+        _______, _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_A   , KC_A   , KC_A   , _______, _______, _______,                     _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_A   , KC_A   , KC_A   , _______, _______, _______,                              _______, _______, _______, _______, _______, _______,
+        _______, KC_A   , KC_A   , KC_A   , _______, _______,                                       _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,TG(AWAY), _______,          _______,                     _______,          _______, _______, _______, _______, _______,
+                                            _______, _______, _______,                     _______, _______, _______,
+    )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -141,10 +215,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-void suspend_power_down_user(void) {
-    rgb_matrix_set_suspend_state(true);
-}
+// void suspend_power_down_user(void) {
+//     rgb_matrix_set_suspend_state(true);
+// }
 
-void suspend_wakeup_init_user(void) {
-    rgb_matrix_set_suspend_state(false);
-}
+// void suspend_wakeup_init_user(void) {
+//     rgb_matrix_set_suspend_state(false);
+// }
